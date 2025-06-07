e0820709ea0fc356e964f3202676cfd2d323da15
Spocat <102896376+Spocat@users.noreply.github.com>
2025-06-07 12:08:15 +0300

    Ball
    добавлен мячик

--- Pingpong.py
+++ Pingpong.py
@@ -1,6 +1,39 @@
 from pygame import *
 
 img_back = ('Ping_background.jpg')
+finish = False
+run = True
+speed_x = 3
+speed_y = 3
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_i] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_k] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 win_width = 700
 win_height = 500
@@ -8,12 +41,27 @@ window = display.set_mode((win_width, win_height))
 display.set_caption('Ping Pong')
 background = transform.scale(image.load(img_back), (win_width, win_height))
 
-run = True
+#racket1 = Player('racket1.png', 30, 200, 4, 50, 150)
+#racket2 = Player('racket2.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     window.blit(background,(0,0))
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        
+        
+        
+        
+        
+        
+        ball.reset()
+
 
     time.delay(50)
     display.update()
